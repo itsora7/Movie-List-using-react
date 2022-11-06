8ee392a9a9aee98aa7b947aad38acec76221af67
Arjun Kumar Shrestha <itsora7@gmail.com>
2022-11-06 14:25:46 +1100

    bug fixed

--- src/utilities/axios-helper.js
+++ src/utilities/axios-helper.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export const fetchData = (str) =>{
     // const apiEP = "https://www.omdbapi.com/?apikey=576ee62b&t=" + str
-    const apiEP = `https://www.omdbapi.com/?apikey=${process.env.REACT_API_KEY}&t=${str}`
+    const apiEP = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&t=${str}`
     console.log(str)
     const response = axios(apiEP)
     // console.log(response)
