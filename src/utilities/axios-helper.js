d05074660ebd4933669d38f3a3540477c373657a
Arjun Kumar Shrestha <itsora7@gmail.com>
2022-11-06 14:13:24 +1100

    Deployment

--- src/utilities/axios-helper.js
+++ src/utilities/axios-helper.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 
 export const fetchData = (str) =>{
     // const apiEP = "https://www.omdbapi.com/?apikey=576ee62b&t=" + str
-    const apiEP = `https://www.omdbapi.com/?apikey=576ee62b&t=${str}`
-    // console.log(str)
+    const apiEP = `https://www.omdbapi.com/?apikey=${process.env.REACT_API_KEY}&t=${str}`
+    console.log(str)
     const response = axios(apiEP)
     // console.log(response)
     return response
